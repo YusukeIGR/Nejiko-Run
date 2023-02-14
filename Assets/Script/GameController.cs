e88cfb5b7591b33a700613343cfb6fda6a7558cb
YusukeIGR <javakunren84@gmail.com>
2023-02-14 13:52:16 +0900

    ~P.288まで

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public NejikoController nejiko;
+    public Text scoreText;
+    public LifePanel lifePanel;
+    public void Update()
+    {
+        int score = CalcScore();
+        scoreText.text="Score:"+score+"m";
+
+        lifePanel.UpdateLife(nejiko.Life());
+
+    }
+
+    int CalcScore(){
+        return (int)nejiko.transform.position.z;
+    }
+}
